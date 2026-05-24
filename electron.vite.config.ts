4ef8c7d884e94a5ec54083a0cf39fc0dada23703
AchuAshwath <achuashwath88@gmail.com>
2026-05-24 16:34:48 +0530

    feat(button): completed shadcn config and added button component

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -5,12 +5,17 @@ import { defineConfig } from "electron-vite";
 export default defineConfig({
   main: {},
   preload: {},
-  renderer: {
-    resolve: {
-      alias: {
-        "@renderer": resolve("src/renderer/src"),
-      },
-    },
+		renderer: {
+			resolve: {
+				alias: {
+					"@renderer": resolve("src/renderer/src"),
+					components: resolve("src/renderer/src/components"),
+					ui: resolve("src/renderer/src/components/ui"),
+					lib: resolve("src/renderer/src/lib"),
+					hooks: resolve("src/renderer/src/hooks"),
+					utils: resolve("src/renderer/src/lib/utils"),
+				},
+			},
     plugins: [react(), tailwindcss()],
   },
 });
