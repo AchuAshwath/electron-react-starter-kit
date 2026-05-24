d5d551a6913f941ee2c2aa55a99ba6461e517a32
AchuAshwath <achuashwath88@gmail.com>
2026-05-24 15:40:15 +0530

    feat(components): added components.json file to bypass tsconfig.json

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,14 +3,14 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "electron-vite";
 
 export default defineConfig({
-	main: {},
-	preload: {},
-	renderer: {
-		resolve: {
-			alias: {
-				"@renderer": resolve("src/renderer/src"),
-			},
-		},
-		plugins: [react()],
-	},
+  main: {},
+  preload: {},
+  renderer: {
+    resolve: {
+      alias: {
+        "@renderer": resolve("src/renderer/src"),
+      },
+    },
+    plugins: [react(), tailwindcss()],
+  },
 });
