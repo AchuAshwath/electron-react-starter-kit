5ed316c53f46c0a58d2a7e5afded59bf1240d0cf
AchuAshwath <achuashwath88@gmail.com>
2026-05-24 16:59:07 +0530

    fix(tailwind): fixed tailwind import error

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "electron-vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   main: {},
