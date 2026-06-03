86b7dd16ccd7daa09a5bff2a411beb179117435a
AchuAshwath <achuashwath88@gmail.com>
2026-06-03 18:58:44 +0530

    feat(storage): add presisted settings store

---
+++ src/main/settings/settings.store.ts
@@ -0,0 +1,44 @@
+import Store from "electron-store";
+import { defaultSettings, type UserSettings } from "./settings.types";
+
+type settingStoreSchema = {
+	settings: UserSettings;
+};
+
+const store = new Store<settingStoreSchema>({
+	defaults: {
+		settings: defaultSettings,
+	},
+	name: "settings",
+});
+
+export function getSettings(): UserSettings {
+	return store.get("settings");
+}
+
+export function updateSettings(patch: Partial<UserSettings>): UserSettings {
+	const currentSettings = getSettings();
+
+	const nextSettings: UserSettings = {
+		...currentSettings,
+		...patch,
+		startup: {
+			...currentSettings.startup,
+			...patch.startup,
+		},
+		windowBounds: {
+			...currentSettings.windowBounds,
+			...patch.windowBounds,
+		},
+	};
+
+	store.set("settings", nextSettings);
+
+	return nextSettings;
+}
+
+export function resetSettings(): UserSettings {
+	store.set("settings", defaultSettings);
+
+	return getSettings();
+}
